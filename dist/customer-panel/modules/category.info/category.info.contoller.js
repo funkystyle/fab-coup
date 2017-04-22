faa535062941e43be55d1f0df6356ff5d0f56c11
suryamuppalla <suryamuppalla@gmail.com>
2017-04-22 16:09:32 -0300

    SEARCH BUTTON MODIFIED IN HEADER ALIGNMENTS MODIFIED IN STORE AND CODE COPIED IN CATAGORY1

--- dist/customer-panel/modules/category.info/category.info.contoller.js
+++ dist/customer-panel/modules/category.info/category.info.contoller.js
@@ -1,1 +1,1 @@
-angular.module("categoryinfoModule",[]).controller("categoryinfoCtrl",["$scope",function(o){console.log("categoryinfoCtrl")}]);
+angular.module("categoryinfoModule",["storeServiceModule"]).controller("categoryinfoCtrl",["$scope","$stateParams","storeService",function(o,e,r){console.log("categoryinfoCtrl"),o.favorite={favorite:!1},o.store=void 0,o.manageFavorite=function(){o.favorite.favorite=!o.favorite.favorite},r.getStore().then(function(e){console.log(e),o.store=e},function(o){console.log("error",o)})}]);
